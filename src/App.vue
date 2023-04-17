473f6ce749fe387ecbe2a36e7f99d0262c866ff1
simretB05 <happysimret@gmail.com>
2023-04-17 05:19:19 -0600

    added SongDisplay vue component to show user selection from list of songs

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <song-display></song-display>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SongDisplay from '@/components/SongDisplay.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SongDisplay
   }
 }
 </script>
---
+++ src/components/SongDisplay.vue
@@ -0,0 +1,139 @@
+<template>
+    <section>
+        <h1>Best of 80's Hits Collections by Various Artists ...</h1>
+            <div class="container">
+                <div class="main_card">
+                    <article  v-for="(song, i) in songs" :key="i" >
+                        <h2 selected_song="song" @click="select_song">{{song[`title`]}}</h2>
+                        <p> Artist Name:{{song[`artist`]}}</p>
+                        <p>song ID:{{song[`song_id`]}}</p>
+                        <img :src="song[`image_url`]" alt="song album image">
+                    </article>
+                </div>
+                <div class="sellection">
+                    <h1 class="sellection_title">Currently playing song</h1>
+
+                    <p class="selected_songs">song title:{{selectedSong}}</p>
+                </div>
+            </div>
+    </section>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                selectedSong: '',
+                songs:[
+                        {
+                            title: "Billie Jean",
+                            artist: "Michael Jackson",
+                            song_id: "123",
+                            image_url:"https://i.ytimg.com/vi/gZRFJyeJLAM/maxresdefault.jpg"
+                        },
+                        {
+                            title: "Sweet Child o' Mine",
+                            artist: "Guns N' Roses",
+                            song_id: "456",
+                            image_url: "https://yt3.googleusercontent.com/XYdITfFzeQBLoNsIIpthkC1jfK9r-jVijx_ULuralq1KaN35Z_tJdgyoB9O-eSCm_NdEgN1E-Ok=s900-c-k-c0x00ffffff-no-rj"
+                        },
+                        {
+                            title: "Like a Prayer",
+                            artist: "Madonna",
+                            song_id: "789",
+                            image_url: "https://i.ytimg.com/vi/IlPCW6nLheM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDFG0GI5nVGiefJK1PwGAW_prr6UA"
+
+                        },
+                        {
+                            title: "Livin' on a Prayer",
+                            artist: "Bon Jovi",
+                            song_id: "1011",
+                            image_url: "https://i.ebayimg.com/images/g/JNIAAOSw62hjGSQb/s-l1600.jpg"
+
+                        },
+                        {
+                            title: "Take On Me",
+                            artist: "a-ha",
+                            song_id: "1213",
+                            image_url: "https://i.ytimg.com/vi/djV11Xbc914/maxresdefault.jpg"
+                        }
+                    ]
+                }
+        },
+    methods: {
+            select_song: function ( details ){
+          
+            let get_title = details[`target`].innerText
+            for ( let i = 0; i<this.songs.length; i++){
+                if ( get_title === this.songs[i][`title`] ){
+                    this.selectedSong = get_title
+                    get_title[`style`][`color`]=`red`
+                    return
+                }
+            }
+            this.selectedSong = 'Please select a song !'
+        },
+    }
+         }
+</script>
+
+<style scoped>
+section{
+    display:grid;
+    place-items: center;
+    min-height: 80vh;
+    
+
+}
+.container{
+    display:grid;
+    place-items: center;
+    min-height: 80vh;
+    width: 100%;
+    grid-template-columns:  1fr 1fr;
+}
+h1{
+    color:red;
+    justify-self: start;
+}
+
+
+.main_card{
+    display:grid;
+    place-items: center;
+    min-height: 100vh;
+    gap:16px;
+
+}
+
+article{
+    display:grid;
+    place-items: center;
+    background-color: lightblue;
+    width: 90%;
+    padding: 16px;
+    min-height: 30%;
+    grid-template-columns: 1fr 1fr 1fr;
+}
+img{
+    width:100px;
+}
+p{
+    text-align: start;
+    padding: 6px;
+    font-weight: 700;
+
+}
+.sellection{
+    min-height: 100vh;
+    align-self: start;
+
+}
+.sellection_title{
+
+    width: 100%;
+}
+.selected_songs{
+     font-size: 1.3rem;
+     color: green;
+}
+</style>
